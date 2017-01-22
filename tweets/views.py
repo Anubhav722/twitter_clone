7ee17039369ebb5de89c621ac2638cba41bd6279
Anubhav_722 <anubhavs286@gmail.com>
2017-01-22 19:18:39 +0530

    Introduction to CBV

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+
 
 from .models import Tweet
 # Create your views here.
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+    # template_name = 'tweets/detail_view.html'
+
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+    # template_name = 'tweets/list_view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context= super(TweetListView, self).get_context_data(*args, **kwargs)
+        print (context)
+        return context
 
 def tweet_detail_view(request, id=1):
     obj = Tweet.objects.get(id=id)
