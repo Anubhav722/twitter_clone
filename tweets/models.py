305944a1f2e85b15fb5ac35bf45ac3e194eacc12
Anubhav_722 <anubhavs286@gmail.com>
2017-01-22 17:55:35 +0530

    add user field to models

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
 
 class Tweet(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     content = models.CharField(max_length=140)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
