b0a06cb33ab674ede6f1a994f791e72d2c7ed4b5
Anubhav_722 <anubhavs286@gmail.com>
2017-01-23 11:39:00 +0530

    learning about validators

--- tweets/forms.py
+++ tweets/forms.py
@@ -10,9 +10,11 @@ class TweetModelForm(forms.ModelForm):
             'content',
         ]
 
-    def clean_content(self, *args, **kwargs):
-        content = self.cleaned_data.get('content')
-        if content == 'abc':
-            raise forms.ValidationError('Cannnot be ABC')
-
-        return content
+    # raising a ValidationError # this one gives directly on the field
+    # def clean_content(self, *args, **kwargs):
+    #     content = self.cleaned_data.get('content')
+    #
+    #     if content == 'abc':
+    #         raise forms.ValidationError('Cannnot be ABC')
+    #
+    #     return content
--- tweets/models.py
+++ tweets/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+
 from django.conf import settings
 
-
+from .validators import validate_content
 # Create your models here.
 
 class Tweet(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    content = models.CharField(max_length=140)
+    content = models.CharField(max_length=140, validators=[validate_content])
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.content)
+
+    # raising a ValidationError also done in forms.py
+    # this one does not give directly on the field
+    # def clean(self, *args, **kwargs):
+    #     content = self.content
+    #     if content == 'abc':
+    #         raise ValidationError("Cannot be ABC")
+    #     return super(Tweet, self).clean(*args, **kwargs)
