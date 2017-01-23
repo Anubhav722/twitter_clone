e06acd4fe0f44eca1425b9f9f518fafe899c72ba
Anubhav_722 <anubhavs286@gmail.com>
2017-01-23 11:23:08 +0530

    Customize the admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Tweet
+from .forms import TweetModelForm
 
-admin.site.register(Tweet)
+class TweetModelForm(admin.ModelAdmin):
+    form = TweetModelForm
+    #
+    # class Meta:
+    #     model = Tweet
+
+admin.site.register(Tweet, TweetModelForm)
