f70e7dd2f0d47a7ea95ffd94f68e114e94416ad4
Anubhav_722 <anubhavs286@gmail.com>
2017-01-24 16:03:19 +0530

    add LoginRequiredMixin in views

--- tweets/views.py
+++ tweets/views.py
@@ -3,18 +3,23 @@ from django.views.generic import DetailView, ListView, CreateView
 
 from .forms import TweetModelForm
 from .models import Tweet
+
+# importing login mixin here
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import FormUserNeededMixin
 # Create your views here.
 
 # learn about this CreateView
-class TweetCreateView(CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
-    success_url = '/tweet/create/'
+    success_url = '/tweet/'
+    login_url = '/admin/'
 
-    def form_valid(self, form):
-        #form.send_email()
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    # def form_valid(self, form):
+    #     #form.send_email()
+    #     form.instance.user = self.request.user
+    #     return super(TweetCreateView, self).form_valid(form)
 
 #learn about DetailView
 class TweetDetailView(DetailView):
@@ -34,7 +39,7 @@ class TweetListView(ListView):
     # by default the name of the template will be tweet_list.html
     def get_context_data(self, *args, **kwargs):
         context= super(TweetListView, self).get_context_data(*args, **kwargs)
-        print (context)
+        #print (context)
         return context
 
 
