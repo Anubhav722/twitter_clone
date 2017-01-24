5fb8bb3aeae7e636177f7d1ae427227f57a38179
Anubhav_722 <anubhavs286@gmail.com>
2017-01-24 12:19:54 +0530

    understanding base views

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from .views import  TweetDetailView, TweetListView, TweetCreateView #tweet_detail_view, tweet_list_view,
+from .views import TweetDetailView, TweetListView, TweetCreateView #tweet_detail_view, tweet_list_view
 
+
+from django.views.generic.base import RedirectView
+# These are base views
+from .views import MyView, HomePageView, TweetCounterRedirectView
 urlpatterns = [
     #url(r'^$', tweet_list_view, name='list'),
     #url(r'^1/$', tweet_detail_view, name='detail'),
@@ -8,4 +12,12 @@ urlpatterns = [
     url(r'^$', TweetListView.as_view() , name='list'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view() , name='detail'),
     url(r'^create/$', TweetCreateView.as_view() , name='create'),
+
+
+    #added to this branch only
+    url(r'^mine/$', MyView.as_view(), name='my-view'),
+    url(r'^home/$', HomePageView.as_view(), name='home-page-view'),
+    url(r'^counter/(?P<pk>[0-9]+)/$', TweetCounterRedirectView.as_view(), name='tweet-counter'),
+    url(r'^go-to-django/$', RedirectView.as_view(url='https://djangoproject.com'), name='go-to-django'),
+
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -3,9 +3,18 @@ from django.views.generic import DetailView, ListView, CreateView
 
 from .forms import TweetModelForm
 from .models import Tweet
+from django.http import HttpResponse
+
+
+# added in this branch
+from django.views import View
+from django.views.generic.base import TemplateView, RedirectView
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
-# learn about this CreateView
+
+
 class TweetCreateView(CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
@@ -16,7 +25,7 @@ class TweetCreateView(CreateView):
         form.instance.user = self.request.user
         return super(TweetCreateView, self).form_valid(form)
 
-#learn about DetailView
+# These here are generic display views
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
     # template_name = 'tweets/detail_view.html'
@@ -66,4 +75,37 @@ class TweetListView(ListView):
 #     }
 #     return render(request, 'tweets/list_view.html', context)
 
+
+# These have been added to understand the class based views
+
+# These here are base views
+class MyView(View):
+    # diff typesof requests can be sent such as post, get, put, patch, delete etc.
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
+
+    # not working correctly. bcz post request requires some input
+    def post(self, request, *args, **kwargs):
+        print ('hi there')
+        return render(request, 'tweets/my_view_test.html', {})
+
+class HomePageView(TemplateView):
+    template_name = "tweets/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['latest_articles'] = Tweet.objects.all()[:5]
+        return context
+
+
+class TweetCounterRedirectView(RedirectView):
+# Whether the redirect should be permanent. The only difference here is the HTTP status code returned. If True, then the redirect will use status code 301. If False, then the redirect will use status code 302. By default, permanent is False.
+    permanent = False
+    query_string = True
+    pattern_name = 'detail' # name of the url to redirect to.
+
+    def get_redirect_url(self, *args, **kwargs):
+        tweet = get_object_or_404(Tweet, pk=kwargs['pk'])
+        #tweet.update_counter()
+        return super(TweetCounterRedirectView, self).get_redirect_url(*args, **kwargs)
 
