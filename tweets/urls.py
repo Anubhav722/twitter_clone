9073a48d1c58c3c51132febde734649337565714
Anubhav_722 <anubhavs286@gmail.com>
2017-02-07 17:03:03 +0530

    add tweet form to homepage/use of widgets Textarea

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from .views import  TweetDetailView, TweetListView, TweetCreateView, TweetUpdateView, TweetDeleteView #tweet_detail_view, tweet_list_view,
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
     #url(r'^$', tweet_list_view, name='list'),
     #url(r'^1/$', tweet_detail_view, name='detail'),
 
-    url(r'^$', TweetListView.as_view() , name='list'),
+    url(r'^$', RedirectView.as_view(url="/")), # this is a nice way to redirect
+    url(r'^search/$', TweetListView.as_view() , name='list'),
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view() , name='detail'),
     url(r'^create/$', TweetCreateView.as_view() , name='create'),
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),
